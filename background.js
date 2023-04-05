21f1838159f5a07831a8325b02e86ffa957b0b2f
Joel Fritschi <95489210+Schugggi@users.noreply.github.com>
2023-04-05 11:27:55 +0200

    adapted persistency to chrome environment

--- background.js
+++ background.js
@@ -7,11 +7,21 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       getCurrentTab().then((tab) => {
         console.log("in promise")
         console.log(tab)
-        // Send a response back to the content script with the current tab information
+        // Send a response back to the content script with the current tab url
         sendResponse({ url: tab.url });
       });
-  
+
       // Return true to indicate that the response will be sent asynchronously
+      return true;
+    }
+
+    // Retreive Color Data & Set Color Data
+    if (message.action === "saveColor") {
+      saveColor(message.url, message.color);
+    } else if (message.action === "getColor") {
+      getColor(message.url, (color) => {
+        sendResponse({ color: color });
+      });
       return true;
     }
   });
@@ -20,4 +30,46 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     let queryOptions = { active: true, lastFocusedWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
     return tab;
+  }
+
+  const isAlreadySaved = (URL, obj) => {
+    for (let i = 0; i < obj.length; i++) {
+      if (obj[i].URL === URL) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  const getIndexOfURL = (URL, obj) => {
+    for (let i = 0; i < obj.length; i++) {
+      if (obj[i].URL === URL) {
+        return i;
+      }
+    }
+  }
+  
+  function saveColor(URL, color) {
+    chrome.storage.local.get("colorData", (data) => {
+      let obj = data.colorData || [];
+      if (isAlreadySaved(URL, obj)) {
+        const index = getIndexOfURL(URL, obj);
+        obj[index].color = color;
+      } else {
+        obj.push({ URL: URL, color: color });
+      }
+      chrome.storage.local.set({ colorData: obj });
+    });
+  }
+  
+  function getColor(URL, callback) {
+    chrome.storage.local.get("colorData", (data) => {
+      let obj = data.colorData || [];
+      if (isAlreadySaved(URL, obj)) {
+        const index = getIndexOfURL(URL, obj);
+        callback(obj[index].color);
+      } else {
+        callback(null);
+      }
+    });
   }
--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,28 @@
 const h2 = document.getElementById("h2url")
-const button = document.getElementById("button")
-let url = null
+const setColorButton = document.getElementById("setColorButton")
+const getColorButton = document.getElementById("getColorButton")
+let url
 
+// Fetch URL from Current Chrome Tab
 chrome.runtime.sendMessage({ action: "getCurrentUrl" }, (response) => {
-    // Handle the response from the background page
-    console.log(response);
-    h2.innerHTML = response.url
-  });
+  console.log(response);
+  h2.innerHTML = response.url
+  url = response.url
+});
 
-function setBackgroundColor(){
-    console.log(h2 ?? null)
-    console.log(url)
-    h2.innerHTML = url
+// Get Color From Chrome Storage
+function getColorClick(){
+  chrome.runtime.sendMessage({action: "getColor", url: url}, (response) => {
+    const color = response.color
+    console.log("Color: ", color)
+  })
 }
 
+// Set Color in Chrome Storage
+function saveColorClick() {
+  console.log("Button Content", h2.innerHTML)
+  chrome.runtime.sendMessage( {action: "saveColor", url: url, color: "#ffffff"})
+}
 
-button.addEventListener("click", setBackgroundColor)
+setColorButton.addEventListener("click", saveColorClick)
+getColorButton.addEventListener("click", getColorClick)
